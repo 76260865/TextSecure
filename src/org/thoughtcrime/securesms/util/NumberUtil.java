aade8e1fc2db8a2a06ab97891a87b7b65d5b3d87
76260865 <76260865@qq.com>
2014-09-23 13:29:33 +0800

    change  textsecure to raymessage

--- src/org/thoughtcrime/securesms/util/NumberUtil.java
+++ src/org/thoughtcrime/securesms/util/NumberUtil.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 Whisper Systems
+ * Copyright (C) 2012 Ray Message
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
